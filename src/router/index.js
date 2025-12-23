e4d69e3e6bcf13378e9a3e4f05045508ceb6941f
SankalpM <sankalpmovva2@gmail.com>
2025-12-23 07:02:00 +0100

    added login routes

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,16 @@ import Home from '../pages/Home.vue';
 import MovieDetail from '../pages/Moviedetail.vue';
 import Account from '../pages/Account.vue';
 import Bookings from '../pages/Booking.vue';
+import Login from '../pages/Login.vue';
+import Register from '../pages/Register.vue';
 
 const routes = [
   { path: '/', component: Home },
   { path: '/movie/:id', component: MovieDetail },
   { path: '/account', component: Account },
   { path: '/bookings', component: Bookings },
+  {path: '/login', component: Login },
+  {path: '/register', component: Register },
 ];
 
 const router = createRouter({
