b9c3d01e3c27351d445aaef017cc6eee60b100b4
SankalpM <sankalpmovva2@gmail.com>
2025-12-23 07:04:11 +0100

    updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,66 @@
+<script setup>
+import { onMounted } from 'vue';
+import { useAuth } from './services/auth';
+
+const { user, logout, loadUser } = useAuth();
+
+onMounted(() => {
+  loadUser();
+});
+</script>
+
 <template>
-  <div id="app">
-    <header class="p-4 bg-gray-800 text-white">
-      <h1 class="text-2xl font-bold">MovieMate</h1>
-    </header>
+  <div>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <router-link class="navbar-brand" to="/">MovieMate</router-link>
 
-    <router-view></router-view> <!-- This is where Home, Booking, etc will appear -->
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-    <footer class="p-4 text-center border-t mt-4">
-      &copy; 2025 MovieMate
-    </footer>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav ms-auto">
+
+            <li class="nav-item">
+              <router-link class="nav-link" to="/">Home</router-link>
+            </li>
+
+            <!-- Logged in -->
+            <template v-if="user">
+              <li class="nav-item">
+                <router-link class="nav-link" to="/account">Account</router-link>
+              </li>
+              <li class="nav-item">
+                <router-link class="nav-link" to="/bookings">Bookings</router-link>
+              </li>
+              <li class="nav-item">
+                <button class="btn btn-outline-warning ms-2" @click="logout">
+                  Logout
+                </button>
+              </li>
+            </template>
+
+            <!-- Logged out -->
+            <template v-else>
+              <li class="nav-item">
+                <router-link class="nav-link btn btn-outline-light ms-2" to="/login">
+                  Login
+                </router-link>
+              </li>
+            </template>
+
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <router-view />
   </div>
 </template>
-
-<script setup>
-// nothing special needed here yet
-</script>
